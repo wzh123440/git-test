5b7bae33553d30cb865ac98b51a91324c0f600b0
wzh <2459425424@qq.com>
2022-09-17 22:40:53 +0800

    hot-fix第一次提交

--- src/main/java/com/wzh/git/GitTest.java
+++ src/main/java/com/wzh/git/GitTest.java
@@ -2,6 +2,7 @@ package com.wzh.git;
 
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("hot fix");
         System.out.println("hello git");
     }
 }
